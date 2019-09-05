d266d53b8660384d8e908ae893e3295a9b68f353
EC2 Default User <ec2-user@ip-172-31-44-196.ap-northeast-1.compute.internal>
2019-09-05 10:55:26 +0000

    タスク管理アプリ_20190905

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,34 +1,58 @@
 class TasksController < ApplicationController
   before_action :set_user
   
+  def index
+    @tasks = @user.tasks
+  end
+  
   def new
     @task = Task.new
   end
   
-  def edit
+  def show
+    @task = Task.find(params[:id])
   end
   
-  def index
-    @tasks = @user.tasks
+  def edit
+    @task = Task.find(params[:id])
   end
   
   def create
     @task = @user.tasks.new(task_params)
     if @task.save
       flash[:success] = 'タスク新規作成に成功しました。'
-      redirect_to user_tasks_path current_user
+      redirect_to user_tasks_url @user
     else
       render :new
     end
   end
   
-  private
-  
-  def set_user
-    @user = User.find(params[:user_id])
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      flash[:success] = " タスクの内容を更新しました。"
+      redirect_to user_task_url(@user, @task)
+    else
+      render :edit      
+    end
   end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.user_id == current_user.id
+      @task.destroy
+      flash[:danger] = " タスクの内容を削除しました"
+      redirect_to user_tasks_path @user
+    end
+  end
+  
+  private
   
   def task_params
     params.require(:task).permit(:task_name, :task_detail)
+  end
+  
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  
+  validates :task_name,  presence: true, length: { maximum: 50 }
+  validates :task_detail, presence: true, length: { maximum: 150 }
 end
